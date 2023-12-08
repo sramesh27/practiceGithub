9e37805d8a71e6d1352eb2722f7acfae71537ec6
Sesslyn041215 <sesslyn2004@gmail.com>
2023-12-08 14:44:42 +0530

    Added vowel count sum

---
+++ vowelCount.js
@@ -0,0 +1,11 @@
+const getVowelCount = (inputStr) => {
+    let totalVowelCount = 0;
+    for(let i=0; i<inputStr.length; i++)
+    {
+        let char = inputStr[i];
+        if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u')
+            totalVowelCount++;
+    }
+    return totalVowelCount;
+}
+console.log(getVowelCount('hello how are you today programiz website?'))
